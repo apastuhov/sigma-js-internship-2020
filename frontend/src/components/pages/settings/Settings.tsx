0a7d306f5829ae078bf88607255efdac33331060
Serhii Kaliuzhnyi <kalyuzhni.sergei@gmail.com>
2020-10-14 13:09:36 +0300

    Added a header & changed a package for material icons

---
+++ frontend/src/components/pages/settings/Settings.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return <h1>Settings</h1>;
+};
+
+export default Settings;
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,4 +1,19 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Chat from '../components/pages/chat/Chat';
+import Login from '../components/pages/login/Login';
+import Profile from '../components/pages/profile/Profile';
+import Register from '../components/pages/registration/Register';
+import Search from '../components/pages/search/Search';
+import Settings from '../components/pages/settings/Settings';
 
-export default () => <Switch></Switch>;
+export default () => (
+  <Switch>
+    <Route path="/" exact component={Profile} />
+    <Route path="/register" component={Register} />
+    <Route path="/chat" component={Chat} />
+    <Route path="/search" component={Search} />
+    <Route path="/settings" component={Settings} />
+    <Route path="/login" component={Login} />
+  </Switch>
+);
