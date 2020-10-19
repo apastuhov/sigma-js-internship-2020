b0275afda455877e45114f62117617d12ae283e9
Roman <canspro1105@gmaiil.com>
2020-10-19 19:36:14 +0300

    Added Tile to Login page

--- frontend/src/components/pages/login/Login.tsx
+++ frontend/src/components/pages/login/Login.tsx
@@ -3,23 +3,27 @@ import Logo from '../../../images/logo-login.svg';
 import GoogleLogo from '../../../images/google-login.svg';
 import './login.scss';
 import Footer from '../../shared/footer/Footer';
+import Tile from '../../shared/tile/Tile';
+import Box from '@material-ui/core/Box';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <div className="login-container">
-        <div className="logo-wrapper">
-          <img src={Logo} alt="logo" className="logo" />
-          <h1>Meetlang</h1>
-        </div>
-        <p className="login-p">Login or register</p>
-        <button className="login-btn">
-          <div className="google-logo-container">
-            <img src={GoogleLogo} alt="google-logo" />
+    <div className="login-wrapper">
+      <Tile>
+        <Box boxShadow={2} className="login-tile">
+          <div className="logo-wrapper">
+            <img src={Logo} alt="logo" className="logo" />
+            <h1>Meetlang</h1>
           </div>
-          <p className="login-btn-p">Sign in with google</p>
-        </button>
-      </div>
+          <p className="login-p">Login or register</p>
+          <button className="login-btn">
+            <div className="google-logo-container">
+              <img src={GoogleLogo} alt="google-logo" />
+            </div>
+            <p className="login-btn-p">Sign in with google</p>
+          </button>
+        </Box>
+      </Tile>
       <Footer />
     </div>
   );
