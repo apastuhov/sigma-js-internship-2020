8a759f6d76b6e96768a567dc26286bcd4ff9d92f
Oleksandr <oleksandr.starov@gmail.com>
2020-11-09 11:33:22 +0200

    Fix type (#109)

--- backend/src/data/settings.ts
+++ backend/src/data/settings.ts
@@ -1,4 +1,4 @@
 export const user = process.env.DB_USER || '';
-export const dbAddress = process.env.DB_NAME || 'localhost';
+export const dbAddress = process.env.DB_ADDRESS || 'localhost';
 export const dbName = process.env.DB_NAME || 'db';
 export const dbPort = process.env.DB_PORT || '27018';
