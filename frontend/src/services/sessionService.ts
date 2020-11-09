b113212146481945e99627208a94d8f47cc4b59c
Oleksandr Starov <oleksandr.starov@leovegas.com>
2020-11-09 13:53:12 +0200

    Add config for FE build

--- frontend/src/services/sessionService.ts
+++ frontend/src/services/sessionService.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { saveUserToStorage } from './localStorageService';
 
-const url = 'http://localhost:8000';
+const url = `${process.env.REACT_APP_API_URL}`;
 
 export const processGoogleResponse = (res: any) => {
   return axios({
     method: 'POST',
-    url: `${url}/api/googleLogin`,
+    url: `${url}/googleLogin`,
     data: { tokenID: res.tokenId }
   }).then(res => {
     const user = res.data;
