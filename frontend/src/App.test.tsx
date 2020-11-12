f2671a75e8db177d57f45c7a37deb00d60cb7b06
Oleksandr <oleksandr.starov@gmail.com>
2020-11-12 12:38:56 +0200

    Added yml to verify code on PR (#125)
    * Added yml to verify code on PR
    
    * Added yml to verify code on PR
    
    * Added yml to verify code on PR
    
    * Added yml to verify code on PR
    
    * Added yml to verify code on PR
    
    * Added yml to verify code on PR
    
    * Update test
    
    * Update test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { ReactNode } from 'react';
 import App from './App';
+import { render, unmountComponentAtNode } from 'react-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./routes/Routes', () => () => <>Routes</>);
+
+jest.mock('./components/storage/friends/friendsContext', () => ({
+  FriendsProvider: ({ children }: { children?: ReactNode }) => <>{children}</>
+}));
+
+describe('App test', () => {
+  let container: any = null;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container!);
+    container?.remove();
+    container = null;
+  });
+
+  it('should render App', () => {
+    render(<App />, container);
+
+    expect(container.querySelector('App')).toBeDefined();
+    expect(container.querySelector('FriendsProvider')).toBeDefined();
+    expect(container.querySelector('Routes')).toBeDefined();
+  });
 });
