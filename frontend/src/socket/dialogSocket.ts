c4e83b4bde20a4d5caa67e93b3b77de35b0847ee
Oleksandr Starov <oleksandr.starov@leovegas.com>
2020-11-18 10:31:18 +0200

    Updated env var key

--- frontend/src/socket/dialogSocket.ts
+++ frontend/src/socket/dialogSocket.ts
@@ -1,22 +1,22 @@
 import io from 'socket.io-client';
 import { IMessage } from '../components/interfaces/Interface';
-const ENDPOINT = `${process.env.REACT_SOCKET_URL}`;
-const socket = io(ENDPOINT)
+const ENDPOINT = `${process.env.REACT_APP_SOCKET_URL}`;
+const socket = io(ENDPOINT);
 
 export const joinDialog = (dialogId: string) => {
   socket.emit('joinDialog', dialogId);
-}
+};
 
 export const leaveRoom = (dialogId: string) => {
   socket.emit('leaveRoom', dialogId);
-}
+};
 
 export const sendSocketMessage = (newMessage: IMessage, dialogId: string) => {
   socket.emit('sendMessage', newMessage, dialogId);
-}
+};
 
 export const getMessage = (newMessage: any) => {
   socket.on('message', (message: IMessage) => {
     newMessage(message);
-  })
-}
+  });
+};
