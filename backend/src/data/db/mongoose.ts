e17dc7d238aa30439257c3386f8a792a5bfebf6e
Vasyl Sava <Vasyard@gmail.com>
2020-11-19 12:36:30 +0200

    Add styles fixes, fix settings functionality (#158)
    * Add styles fixes, fix settings functionality
    
    * Add syntax fixes

--- backend/src/data/db/mongoose.ts
+++ backend/src/data/db/mongoose.ts
@@ -5,11 +5,15 @@ const dbUser = user ? `${user}:${user}@` : '';
 
 const dbLink = `mongodb://${dbUser}${dbAddress}:${dbPort}/${dbName}`;
 
-mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, err => {
-  if (err) {
-    console.error('Error occured during connection to DB');
-    console.error(err);
-  } else {
-    console.log('Connected to DB succesfully');
+mongoose.connect(
+  dbLink,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false },
+  err => {
+    if (err) {
+      console.error('Error occured during connection to DB');
+      console.error(err);
+    } else {
+      console.log('Connected to DB succesfully');
+    }
   }
-});
+);
