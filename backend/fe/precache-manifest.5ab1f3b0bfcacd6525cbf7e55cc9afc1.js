2946ee2e3a71d125630268aefdc5b623f50ecf2f
actions <gitactions@gmail.com>
2020-11-20 13:18:22 +0000

    release

---
+++ backend/fe/precache-manifest.5ab1f3b0bfcacd6525cbf7e55cc9afc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7eb5b6c1105fff8dc9f25dcb4a099fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166ec8b1afceec600993",
+    "url": "/static/css/main.ca6172ca.chunk.css"
+  },
+  {
+    "revision": "15c93652212c5ac02f13",
+    "url": "/static/js/2.df340e27.chunk.js"
+  },
+  {
+    "revision": "77712f4822705f4521f4d3c5ff167a28",
+    "url": "/static/js/2.df340e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166ec8b1afceec600993",
+    "url": "/static/js/main.1d528ab0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1bad09c155b7289ccaa7f4d404038663",
+    "url": "/static/media/google-login.1bad09c1.svg"
+  },
+  {
+    "revision": "41fa017a359322d955625fb5f97d8f1b",
+    "url": "/static/media/logo-login.41fa017a.svg"
+  }
+]);
--- frontend/src/services/postService.ts
+++
@@ -1,5 +0,0 @@
-import { dataType } from '../components/interfaces/Interface';
-import { getRequest, postRequest } from './apiService';
-
-export const getUserPosts = (id?: string) => getRequest(`${dataType.user}/${id}/${dataType.posts}`);
-export const sendPostToUser = (id: string, body: {}) => postRequest(`${dataType.user}/${id}/${dataType.posts}`, body);
